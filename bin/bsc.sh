0dd47c45dd4f019ef56669350f558b8f11456a2b
Tuan Anh Nguyen <anhntbk08@gmail.com>
2021-04-15 10:36:05 +0700

    Delete bin directory

--- bin/bsc.sh
+++
@@ -1,4 +0,0 @@
-
-npx ganache-cli   -i 56 \
-    --chainId 56 \
-    -l 100000000  --fork https://bsc-dataseed.binance.org/ -m "seven kitten transfer domain apple news struggle author eye pet mule ozone"
